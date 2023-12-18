b8a29719ae9e0eca604afab8e439c63227229134
Pradeep Venkatesh <Pradeep Venkatesh@TYSS-Pradeep-Venkatesh>
2023-12-18 12:10:12 +0530

    1st push

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Pradeep Venkatesh
+ *
+ */
+module FacebookTesting {
+}
---
+++ src/tesingFacebook/Login.java
@@ -0,0 +1,7 @@
+package tesingFacebook;
+
+public class Login {
+	public static void main(String[] args) {
+		System.out.println("Hii...");
+	}
+}
